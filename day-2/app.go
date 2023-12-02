862200b0bff3f8fdc13723b2c86bb5fe62865984
Karl Sonley <karlsonley@gmail.com>
2023-12-02 15:45:47 +0100

    Day 2 Part 2

--- day-2/app.go
+++ day-2/app.go
@@ -54,6 +54,51 @@ func partOne() {
 	fmt.Println("Game 1:", sum)
 }
 
+func partTwo() {
+	file, err := os.Open("resources/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	colourCountsRex := regexp.MustCompile("(\\d+) ([a-z]+)")
+
+	sum := 0
+
+	for scanner.Scan() {
+		minGame := map[string]int{
+			"red":   0,
+			"green": 0,
+			"blue":  0,
+		}
+
+		colourCountsMatches := colourCountsRex.FindAllStringSubmatch(scanner.Text(), -1)
+
+		for i := range colourCountsMatches {
+			count, _ := strconv.Atoi(colourCountsMatches[i][1])
+			colour := colourCountsMatches[i][2]
+
+			if minGame[colour] < count {
+				minGame[colour] = count
+			}
+		}
+
+		product := 1
+
+		for _, value := range minGame {
+			product *= value
+		}
+
+		sum += product
+	}
+
+	fmt.Println("Game 2:", sum)
+}
+
 func main() {
 	partOne()
+	partTwo()
 }
